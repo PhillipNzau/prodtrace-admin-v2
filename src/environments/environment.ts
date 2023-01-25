393dcd23cddf86456dd90b8ef9cfe957c07f9752
Phillip Nzau <philip.nzau@8teq.co.ke>
2023-01-25 13:21:30 +0000

    Update src/environments/environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,9 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 // const url = 'http://192.168.1.24:8000/api/v1/';
-const url = 'http://192.168.0.123:8000/api/v1/';
+// const url = 'http://192.168.0.123:8000/api/v1/';
+const url = 'https://staging.avl.local:4443/api/v1/';
+
 
 const userBaseUrl = url + 'user/';
 const farmBaseUrl = url + 'farm/';
@@ -13,7 +15,7 @@ const geoCoderUrl = 'https://nominatim.openstreetmap.org/search?format=json&addr
 // const geoCoderUrl = 'http://139.180.192.124:8080/';
 export const environment = {
   mapbox: {
-    accessToken: 'pk.eyJ1IjoiYnJhc2thbSIsImEiOiJja3NqcXBzbWoyZ3ZvMm5ybzA4N2dzaDR6In0.RUAYJFnNgOnn80wXkrV9ZA',
+    accessToken: 'pk.eyJ1IjoicGhpbGxpcDc4IiwiYSI6ImNsZGJsYzQxcDAxcHMzb3E1NW80Nzg5YWoifQ.4_d9rNJlocMnH6klTZaa9Q',
   },
   production: false,
 
