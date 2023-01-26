bcd03ffe26e021d49988443b6e0a7b4ca43be98f
stve997 <stevemwalili54@gmail.com>
2023-01-26 14:37:41 +0300

    fix

--- admin-setup.sh
+++ admin-setup.sh
@@ -1,6 +1,6 @@
 if [ -d "/home/jenkins/prodtrace-admin" ];
 then
-    rm -r prodtrace-frontend/
+    rm -r prodtrace-admin/
     scp -r jenkins@192.168.1.22:/var/lib/jenkins/workspace/prodtrace-admin .
     #git clone http://stve11:xQPoYMpFPYyJWpgmCjnm@192.168.0.207/naiparq/naiparq-backend-pms.git
 else
