9f53a1cc2434d520feb5c66247a181f4d48cac7a
stve997 <stevemwalili54@gmail.com>
2023-01-26 13:03:28 +0300

    bash syntax error

--- admin-setup.sh
+++ admin-setup.sh
@@ -6,3 +6,4 @@ then
 else
     scp -r jenkins@192.168.1.22:/var/lib/jenkins/workspace/prodtrace-admin .
     #git clone http://stve11:xQPoYMpFPYyJWpgmCjnm@192.168.0.207/naiparq/naiparq-backend-pms.git
+fi
