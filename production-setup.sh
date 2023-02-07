456353e9cac185014ae11db6014321cc708e619c
stve997 <stevemwalili54@gmail.com>
2023-02-07 13:05:13 +0300

    fix

--- production-setup.sh
+++ production-setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scp -r /var/lib/jenkins/workspace/prodtrace-admin jenkins@45.76.214.70:/home/jenkins/
+scp -r /var/lib/jenkins/workspace/prodtrace-admin jenkins@45.76.214.70:/home/jenkins
