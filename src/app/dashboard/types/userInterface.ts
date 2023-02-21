59bb39e151edd6a76c95b6bb37c2d51f8ab53c39
kwoshvick <kwoshvick@gmail.com>
2023-02-21 15:20:30 +0300

    removed unnecessary files

--- src/app/dashboard/types/userInterface.ts
+++
@@ -1,9 +0,0 @@
-export interface UserInterface {
-  id: string,
-  first_name: string,
-  last_name: string,
-  email: string,
-  phone_number: string,
-  last_login: string,
-  user_type_id: number,
-}
