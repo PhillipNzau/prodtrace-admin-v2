fdf2a1c772fa1e89060f51aae3d88aed19342d95
kwoshvick <kwoshvick@gmail.com>
2023-02-21 16:55:11 +0300

    changes

--- prodtrace-admin-deployment-prod.sh
+++ prodtrace-admin-deployment-prod.sh
@@ -6,7 +6,7 @@ if [ -d "$CURRENT_FOLDER" ]; then
     docker-compose -f $DOCKER_COMPOSE down
     mkdir -p $PREVIOUS_FOLDER
     mv $DOCKER_COMPOSE $PREVIOUS_FOLDER
-    docker rmi -f 45.32.22.181:5000/prodtrace-backend:latest
+    docker rmi -f 45.32.22.181:5000/prodtrace_admin:latest
 
 else
   mkdir -p $CURRENT_FOLDER
--- prodtrace-admin-deployment-staging.sh
+++ prodtrace-admin-deployment-staging.sh
@@ -6,7 +6,7 @@ if [ -d "$CURRENT_FOLDER" ]; then
     docker-compose -f $DOCKER_COMPOSE down
     mkdir -p $PREVIOUS_FOLDER
     mv $DOCKER_COMPOSE $PREVIOUS_FOLDER
-    docker rmi -f 192.168.1.23:5000/prodtrace-backend:latest
+    docker rmi -f 192.168.1.23:5000/prodtrace_admin:latest
 
 else
   mkdir -p $CURRENT_FOLDER
