dcd3b0d3a75de98017dae5dad0c23b7289c57aaf
stve997 <stevemwalili54@gmail.com>
2023-02-21 10:36:49 +0300

    setup script

--- staging-setup.sh
+++ staging-setup.sh
@@ -1,9 +1,13 @@
-if [ -d "/home/jenkins/prodtrace-admin" ];
-then
-    rm -r prodtrace-admin/
-    scp -r jenkins@192.168.1.22:/var/lib/jenkins/workspace/prodtrace-admin .
+#!/bin/bash
+
+#if [ -d "/home/jenkins/prodtrace-admin" ];
+#then
+#    rm -r prodtrace-admin/
+#    scp -r jenkins@192.168.1.22:/var/lib/jenkins/workspace/prodtrace-admin .
     #git clone http://stve11:xQPoYMpFPYyJWpgmCjnm@192.168.0.207/naiparq/naiparq-backend-pms.git
-else
-    scp -r jenkins@192.168.1.22:/var/lib/jenkins/workspace/prodtrace-admin .
+#else
+#    scp -r jenkins@192.168.1.22:/var/lib/jenkins/workspace/prodtrace-admin .
     #git clone http://stve11:xQPoYMpFPYyJWpgmCjnm@192.168.0.207/naiparq/naiparq-backend-pms.git
-fi
+#fi
+
+rsync -av --exclude=.git /var/lib/jenkins/workspace/prodtrace-admin jenkins@192.168.1.24:/home/jenkins
